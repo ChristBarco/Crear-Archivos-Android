af578aac8a309823aab451f18dd9853c72a21751
ChristBarco <barcochrist@gmail.com>
2017-01-29 08:24:29 -0500

    Creacion de archivo en directorio del almacenamiento externo, ya sea en la memoria externa o interna del cel

--- app/src/main/java/com/programacionmaster/almacenararchivos/MainActivity.java
+++ app/src/main/java/com/programacionmaster/almacenararchivos/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,19 +36,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void crearDirectorioArchivo() {
         if (isExternalStorageWritable() && isExternalStorageReadable()) {
-            crearDirectorio(this, "base_datos");
+
+            if (Environment.isExternalStorageRemovable()) {
+                Log.i("isExtStorRemovable", "true");
+            } else {
+                Log.i("isExtStorRemovable", "false");
+                File directorio = crearDirectorio(this, "base_datos");
+                crearArchivo(this, "archivo_prueba", "Cuerpo de archivo Prueba", directorio);
+            }
         } else {
             Log.e("ErrorExternal", "Almacenamiento externo no tiene permisos");
         }
     }
 
+    /**
+     * Crea un directorio en el almacenamiento externo.
+     *
+     * @param context
+     * @param nombre
+     * @return
+     */
     public File crearDirectorio(Context context, String nombre) {
         File file = new File(context.getExternalFilesDir(
-                Environment.DIRECTORY_DOWNLOADS), nombre);
+                Environment.DIRECTORY_DOCUMENTS), nombre);
         if (!file.mkdirs()) {
             Log.e(LOG_TAG, "Directorio no creado");
         }
         return file;
+    }
+
+    /**
+     * Crea archivo de texto plano en un directorio especificado.
+     *
+     * @param context
+     * @param nombre
+     * @param cuerpo
+     * @param directorio
+     */
+    public void crearArchivo(Context context, String nombre, String cuerpo, File directorio) {
+        try {
+            File gpxfile = new File(directorio, nombre);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(cuerpo);
+            writer.flush();
+            writer.close();
+            Log.i("CreacionArchivo", "Archivo Creado en: " + directorio.getAbsolutePath());
+            Toast.makeText(context, "Archivo Creado en: " + directorio.getAbsolutePath(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
